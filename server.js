75ee23ea9706d22649a94ca0b539f15f99279830
Toby Crawley <toby@tcrawley.org>
2017-03-21 15:30:44 -0400

    Add timeouts, proper promise handling, etc

--- server.js
+++ server.js
@@ -2,81 +2,96 @@ const fs   = require('fs'),
       http = require('http'),
       url  = require('url');
 
-// TODO: optional
-const packageJson = JSON.parse(fs.readFileSync('package.json'));
+// TODO: better logging
 
-const functions = require("./" + (packageJson.main === undefined ? 'index.js' : packageJson.main));
+var functionsFile;
+if (fs.existsSync('package.json')) {
+  functionsFile = JSON.parse(fs.readFileSync('package.json')).main;
+}
+
+const functions = require("./" + (functionsFile === undefined ? 'index.js' : functionsFile));
+
+const currentTime = () => Date.now();
 
 const server = http.createServer(function(request, response) {
 
+  // TODO: make configurable
+  const timeout = 500;
+  var timeoutTimer;
+  
   const callback = (status, body, headers) => {
+    if (timeoutTimer !== undefined) {
+      clearTimeout(timeoutTimer);
+      timeoutTimer = null;
+    }
+    
+    if (response.finished) {
+      
+      return;
+    }
+    
     if (status === undefined) {
       status = 200;
     }
     response.writeHead(status, headers);
-
+    
     if (body !== undefined) {
       response.write(body);
     }
-
+    
     response.end();
   };
-
-  // TODO: support module.exports = function in addition to a map of
-  // functions
   
-  const fName = url.parse(request.url).pathname.split('/')[1];
-  if (fName === undefined) {
-    callback(400, "No function name provided");
-
-    return;
-  }
-
-  const fn = functions[fName];
-  if (fn === undefined) {
-    callback(404, `No fn named '${fName}' available`);
-
-    return;
+  var fn;
+  var fName = "";
+  if (typeof(functions) === "object") {
+    fName = url.parse(request.url).pathname.split('/')[1];
+    if (fName === undefined) {
+      callback(400, "No function name provided");
+      
+      return;
+    }
+    
+    fn = functions[fName];
+    if (fn === undefined) {
+      callback(404, `No fn named '${fName}' available`);
+      
+      return;
+    }
+  } else {
+    fn = functions;
   }
 
   const context = {
+    callback: callback,
     request: request,
     response: response,
-    functionName: fName
+    functionName: fName,
+    remainingTime: () => startTime + timeout - currentTime()
     // TODO: more context, like config/secrets/etc
   };
 
   const errHandler = (ex) => {
-        console.log(`Error invoking '${fName}': ${ex}`);
-        callback(500, `Error invoking function '${fName}'`);
+    console.log(`Error invoking '${fName}': ${ex}`);
+    callback(500, `Error invoking function '${fName}'`);
   };
 
-  const invalidSig = () => callback(500, `Function '${fName}' has an`
-                                    + ` invalid signature`);
-  
-  switch (fn.length) {
-  case 0:
-    invalidSig();
-    break;
+  const startTime = currentTime();
+  timeoutTimer = setTimeout(callback, timeout, 504, `Invoking function '${fName}' timed-out after ${timeout}ms`);
+
+  try {
+    const maybePromise = fn(context);
     
-  case 1:
-    Promise.resolve(fn(context)).
-      then(({status, body, headers}) => callback(status, body, headers)).
-      catch(errHandler);
-    break;
-
-  case 2:
-    try {
-      fn(context, callback);
-    } catch (ex) {
-      errHandler(ex);
+    if (maybePromise !== undefined &&
+        typeof(maybePromise.then) === "function") {
+      maybePromise.
+        then(({status, body, headers}) => callback(status, body, headers)).
+        catch(errHandler);
     }
-    break;
-
-  default:
-    invalidSig();
+  } catch (ex) {
+    errHandler(ex);
   }
 });
-
+                                
 server.listen(8080);
 console.log("Listening on 8080");
